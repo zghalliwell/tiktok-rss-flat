41ee12332c9010620c853b4bceceb6a38d7224e2
Zach Halliwell <44303844+zghalliwell@users.noreply.github.com>
2025-02-24 09:00:53 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Set your GH Pages URL here e.g.:
 ghPagesURL = "https://zghalliwell.github.io/tiktok-rss-flat/"
-ghRawURL = "https://raw.githubusercontent.com/conoro/tiktok-rss-flat/main/"
+ghRawURL = "https://zghalliwell.github.io/tiktok-rss-flat/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
